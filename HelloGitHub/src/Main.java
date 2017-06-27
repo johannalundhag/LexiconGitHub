5c54b12601506fd1d193754e2ca0e3b397abd77a
Administrator <Administrator@DESK-FDCJBB2.lexicon.edu.local>
2017-06-27 14:23:36 +0200

    First addition

--- HelloGitHub/src/Main.java
+++ HelloGitHub/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Albin");
 
+		Person p1=new Person("Albin", 12);
+		
+		System.out.println(p1.toString());
 	}
 
 }
